ce34c4dea7267fa593a835b8f96159aac829a42e
Kyosangjeon <65115151+Kyosangjeon@users.noreply.github.com>
2020-06-23 23:39:46 +0900

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from score_management_system import ScoreManagementSystem
-		
-
-if __name__ == "__main__":sms = ScoreManagementSystem()
-sms.read('score.csv')
-print(sms.sort("avg","des"))
-sms.write('result.csv',"avg","des") 
